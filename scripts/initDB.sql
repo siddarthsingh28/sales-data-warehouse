921d7c0ad889288ea61bc25757284ce956590587
Siddarth Singh <73015518+siddarthsingh28@users.noreply.github.com>
2025-03-10 12:30:23 +0530

    Update initDB.sql

--- scripts/initDB.sql
+++ scripts/initDB.sql
@@ -1,3 +1,9 @@
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
 create Database DataWarehouse
 
 
